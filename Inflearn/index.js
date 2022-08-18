c9f67af8643eb5851556f936bf11f1fb41ba0690
yusiny <yusinkim.or@gmail.com>
2022-08-18 16:35:17 +0900

    [Inflearn] 1- 메인 페이저 인디케이서 적용 (1)

--- Inflearn/index.js
+++ Inflearn/index.js
@@ -1,6 +1,16 @@
 window.onload = () => {
+
     const swiper = new Swiper('.swiper', {
         direction: 'horizontal',
-        loop: true
+        loop: true,
+        navigation: {
+            nextEl: "#swiper-button-next",
+            prevEl: "#swiper-button-prev",
+        }
     });
+
+    swiper.on('transitionEnd', function() {
+        document.getElementById("main_banner_pagination_idx").innerHTML = `${swiper.realIndex + 1}/3`;
+    });
+    
 }
