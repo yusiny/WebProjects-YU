4ab8f0f6b8a2c45fce25e77279dadfedfc44bda2
yusiny <yusinkim.or@gmail.com>
2022-09-03 01:27:30 +0900

    [react-inflearn] 하단 배너 .json

--- react-inflearn/src/assets/banners.js
+++ react-inflearn/src/assets/banners.js
@@ -95,3 +95,31 @@ export const mainBanners = [
     background_color: "#00c471",
   },
 ];
+
+export const bottomBanners = [
+  {
+    background_url:
+      "https://cdn.inflearn.com/public/main_sliders/3b67a1a1-aae3-442e-8e17-791b7cdfb5f5/bottom-1920-205-%281111%29.jpg",
+    title:
+      "모든 팀원이 인프런의 강의들을 \n자유롭게 학습하는 환경을 제공해 주세요.",
+    button: "비즈니스 알아보기",
+    text_color: "#000",
+  },
+
+  {
+    background_url:
+      "https://cdn.inflearn.com/public/main_sliders/38744ba0-b12a-491a-9ca2-43375c3905ba/share-you.jpg",
+    title: "지식을 나눠주세요. \n쉽게 시작하고 합당한 보상을 받을 수 있어요.",
+    button: "지식공유 알아보기",
+    text_color: "#000",
+  },
+
+  {
+    background_url:
+      "https://cdn.inflearn.com/public/main_sliders/39bcb91f-64a4-4a16-8878-e8dc7cd342e8/bottom-1920-205-%28221%29.jpg",
+    title:
+      "당신은 더 좋은 곳에 갈 수 있어요. \n지금 열려있는 채용공고를 확인해보세요.",
+    button: "공고 확인하기",
+    text_color: "#fff",
+  },
+];
